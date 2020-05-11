3c6321b229515be2fff67800fc8b3635d500e689
duffy_t_ <tsu.to.mu.t.0623@gmail.com>
2020-05-11 13:57:04 +0900

    メタファイル上げなおし

--- src/main/java/com/arkinfosys/salezy/domain/security/AccessInfo_.java
+++ src/main/java/com/arkinfosys/salezy/domain/security/AccessInfo_.java
@@ -1,20 +1,18 @@
 package com.arkinfosys.salezy.domain.security;
 
+import com.arkinfosys.salezy.domain.core.BaseEntity_;
+import com.arkinfosys.salezy.domain.user.UserInfo;
 import java.sql.Timestamp;
-
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-import com.arkinfosys.salezy.domain.core.BaseEntity_;
-import com.arkinfosys.salezy.domain.user.UserInfo;
-
-@Generated(value="Dali", date="2020-05-11T13:50:36.656+0900")
+@Generated(value="Dali", date="2020-05-11T13:53:37.063+0900")
 @StaticMetamodel(AccessInfo.class)
 public class AccessInfo_ extends BaseEntity_ {
-    public static volatile SingularAttribute<AccessInfo, String> accessKey;
-    public static volatile SingularAttribute<AccessInfo, UserInfo> userInfo;
-    public static volatile SingularAttribute<AccessInfo, Integer> usedCount;
-    public static volatile SingularAttribute<AccessInfo, String> remoteAddress;
-    public static volatile SingularAttribute<AccessInfo, Timestamp> registrationTime;
+	public static volatile SingularAttribute<AccessInfo, String> accessKey;
+	public static volatile SingularAttribute<AccessInfo, UserInfo> userInfo;
+	public static volatile SingularAttribute<AccessInfo, Integer> usedCount;
+	public static volatile SingularAttribute<AccessInfo, String> remoteAddress;
+	public static volatile SingularAttribute<AccessInfo, Timestamp> registrationTime;
 }
